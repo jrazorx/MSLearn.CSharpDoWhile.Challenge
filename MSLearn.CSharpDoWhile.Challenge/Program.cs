94a15f7827fe3e40d9f9dd484e006954da4a1419
jra <julien.gault.50@gmail.com>
2024-02-27 17:46:28 +0100

    Add project files.

---
+++ MSLearn.CSharpDoWhile.Challenge/Program.cs
@@ -0,0 +1,24 @@
+﻿int heroHealth      = 10;
+int monsterHealth   = 10;
+
+bool isHeroTurn = true;
+
+Random random = new();
+int damage = 0;
+
+do
+{
+    damage = random.Next(1, 11);
+
+    if (isHeroTurn)
+        monsterHealth -= damage;
+    else
+        heroHealth -= damage;
+
+    Console.WriteLine((isHeroTurn ? "Monster" : "Hero") + " was damaged and lost " + damage + " health and now has " + (isHeroTurn ? monsterHealth : heroHealth) + " health.");
+
+    isHeroTurn = !isHeroTurn;
+
+} while (monsterHealth > 0 && heroHealth > 0);
+
+Console.WriteLine(isHeroTurn ? "Monster wins !" : "Hero wins !");
